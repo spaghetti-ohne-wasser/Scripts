b827185cfa026e8ba9d664f3288d6ba36fd8d9e8
Spaghetti without Water <105888772+spaghetti-ohne-wasser@users.noreply.github.com>
2022-06-23 19:37:34 +0200

    Update Network-Scanner.py

--- Network-Scanner.py
+++ Network-Scanner.py
@@ -1,30 +1,19 @@
 import subprocess
-import platform
-import threading
 
-if not platform.system().lower() == "linux":
-    print("the script only works under linux(parrot)")
-    exit(1)
-decoding: str = "utf-8"
-net_part: str = ".".join(subprocess.check_output(["hostname","-I"]).decode(decoding).split(" ")[0].split(".")[0:3])
+network_portion: str = ".".join(
+    subprocess.check_output(["hostname", "-I"]).decode(encoding="utf-8").split(" ")[0].split(".")[0:3])
 
 
-def check_device(host_portion: str):
-    ip: str = net_part + "." + host_portion
-    command: str = "ping " + ip + " -c 1"
+def check_address(host_portion: str):
+    ip4: str = network_portion + "." + host_portion
     try:
-        output: str = subprocess.check_output(command.split(" ")).decode(decoding)
-        ttl: int = int(output.split(" ")[11].split("ttl=")[1])
-        if ttl <= 64 and ttl >= 32:
-            os: str = "Unix/Linux"
-        elif ttl >= 64 and ttl <= 128:
-            os: str = "Windows"
-        else:
-            os: str = "Solaris/AIX"
-        print("Device found " + ip + " " + os + " " + subprocess.check_output(["date"]).decode(decoding).strip())
-    except subprocess.CalledProcessError:
+        output: str = subprocess.check_output(["nslookup", ip4]).decode(encoding="utf-8")
+        name: str = output.split("name = ")[1].strip().replace("\n", " ")
+        print("Device found " + ip4, name)
+    except:
         return
 
+
 if __name__ == "__main__":
-    for index in range(1, 255):
-        threading.Thread(target=lambda: check_device(str(index))).start()
+    for index in range(256):
+        check_address(str(index))
